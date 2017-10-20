ef5b7052c5208739244c37c5234a76b886e07cf4
Az928 <TheAz928@users.noreply.github.com>
2017-10-21 00:53:29 +0600

    Add files via upload

--- src/ItemCommands/Core.php
+++ src/ItemCommands/Core.php
@@ -1,1 +1,57 @@
+<?php
+namespace ItemCommands;
 
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+
+class Core extends PluginBase implements Listener{
+	
+	/* @var items (array)
+	 * contains Item::claes (Not pocketmine Item::class)
+	 */
+	public $items = [];
+	
+	public function onLoad(){
+		 $this->saveDefaultConfig();
+	    $this->getLogger()->info("§7Loading items...");
+	    foreach($this->getConfig()->getAll() as $stringKey => $data){
+	       $this->items[$stringKey] = new Item($stringKey, isset($data["lore"]) ? $data["lore"] : [], $data["name"], $data["commands"], $data["runAs"]);
+	       $this->getLogger()->info("§2Loaded ".$this->items[$stringKey]->getName());
+	    }
+	    $this->getLogger()->info("§aInit process done!");
+	}
+	
+	public function onEnable(){
+	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+	    $this->getLogger()->info("§6ItemCommands has been enabled successfully!");
+	}
+	
+	/*
+	 * @void onHeld
+	 * PlayerItemHeldEvent $event
+	 * Priority: HIGHEST
+	 */
+	
+	public function onHeld(PlayerItemHeldEvent $event){
+	    $player = $event->getPlayer();
+	    foreach($this->items as $key => $class){
+	       $class->keepChecking($player);
+	    }
+	}
+	
+	/*
+	 * @void onTap
+	 * PlayerInteractEvent $event
+	 * Priority: HIGHEST
+	 */
+	
+	public function onTap(PlayerInteractEvent $event){
+	    $player = $event->getPlayer();
+	    foreach($this->items as $key => $class){
+	       $class->checkExecution($player);
+	    }
+	}
+}
---
+++ src/ItemCommands/Item.php
@@ -0,0 +1,176 @@
+<?php
+namespace ItemCommands;
+
+use pocketmine\item\Item as IT;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+/* 
+ * Developed by TheAz928
+ * ItemCommands handler class
+ * This software is under GNU General Public License v3.0.0 and later
+ */
+
+use pocketmine\command\ConsoleCommandSender;
+
+class Item{
+
+   CONST AS_PLAYER = 0;
+
+   CONST AS_OP = 1;
+
+   CONST AS_CONSOLE = 2;
+	
+	/* @var stringKey (string) null|id:damage */
+	private $stringKey = "";
+	
+	/* @var lore (array) */
+	private $lore = [];
+	
+	/* @var name (string) */
+	private $name = "";
+	
+	/* @var commands (array) */
+	private $commands = [];
+	
+	/* @var runType (Int) */
+	private $runType = 0;
+	
+	/*
+	 * Item(CommandItem) constructor
+	 *
+	 * String $stringKey
+	 * Array $lore
+	 * String $name
+	 * Array $commands
+	 * String $runType
+	 */
+	
+	public function __construct(string $stringKey, array $lore, string $name, array $commands, Int $runType){
+	    $this->stringKey = $stringKey;
+       $this->lore = $lore;
+       $this->name = $name;
+       $this->commands = $commands;
+       $this->runType = $runType > 2 or $runType < 0 ? 0 : $runType;
+	}
+	
+	/*
+	 * @void getItem
+	 * return Item
+	 */
+	
+	public function  getItem(): IT{
+	    $data = explode(":", $this->stringKey);
+	    $meta = isset($data[1]) ? $data[1] : 0;
+	    $item = IT::get($data[0], $meta, 1);
+	return $item;
+	}
+	
+	/*
+	 * @void getLore
+	 * return array
+	 */
+	
+	public function getLore(): array{
+	    $lore = [];
+	    foreach($this->lore as $lor){
+	       $lore[] = "§7".$lor."§r";
+	    }
+	return $lore;
+	}
+	
+	/*
+	 * @void getName
+	 * return string
+	 */
+	
+	public function getName(): string{
+	    $name = "§r".$this->name."§r";
+	return $name;
+	}
+	
+	/*
+	 * @void getCommands
+	 * Player $player
+	 * return array
+	 */
+	
+	public function getCommands(Player $player): array{
+	    $cmds = [];
+	    foreach($this->commands as $key => $cmd){
+	      $cmds[] = str_replace(["{name}", "{tag}", "{level}", "{x}", "{y}", "{z}"], [$player->getName(), $player->getNameTag(), $player->getLevel()->getName(), $player->x, $player->y, $player->z], $cmd);
+	    }
+	return $cmds;
+	}
+	
+	/*
+	 * @void getRunType
+	 * return string
+	 */
+	
+	public function getRunType(): Int{
+	    return $this->runType;
+	}
+	
+	/*
+	 * @void keepChecking
+	 * Player $player
+	 *
+	 * Checks player's inventory
+	 * if there is command item
+	 * but it doesn't have proper 
+	 * name or lore
+	 *
+	 */
+	
+	public function keepChecking(Player $player): void{
+		 $check = $this->getItem();
+	    foreach($player->getInventory()->getContents() as $slot => $item){
+	       if($item->getId() == $check->getId() and $item->getDamage() == $check->getDamage()){
+		      if($item->getName() !== $this->getName() or $item->getLore() !== $this->getLore()){
+			     $item->setCustomName($this->getName());
+			     $item->setLore($this->getLore());
+			     $player->getInventory()->setItem($slot, $item);
+			    }
+		    }
+	    }
+	}
+	
+	/*
+	 * @void checkExecution
+	 * Player $player
+	 *
+	 */
+	
+	public function checkExecution(Player $player): void{
+	    $inv = $player->getInventory();
+	    $item = $inv->getItemInHand();
+	    $check = $this->getItem();
+	    if($item->getId() == $check->getId() and $item->getDamage() == $check->getDamage()){
+		   if($item->getName() == $this->getName()){ # No need to check lore
+			  $commands = $this->getCommands($player);
+			  foreach($commands as $cmd){
+			    if($this->getRunType() == self::AS_PLAYER){
+				   Server::getInstance()->dispatchCommand($player, $cmd);
+				 }
+				 if($this->getRunType() == self::AS_OP){
+					if($player->isOp()){
+				     Server::getInstance()->dispatchCommand($player, $cmd);
+				   }else{
+					  $player->setOp(true);
+				     Server::getInstance()->dispatchCommand($player, $cmd);
+				     $player->setOp(false);
+				   }
+				 }
+				 if($this->getRunType() == self::AS_CONSOLE){
+				   Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), $cmd);
+				 }
+			  }
+			  $item->setCount(1);
+			  $inv->removeItem($item);
+			  $player->sendTip("§7You've used ".$this->getName());
+			}
+		}
+	}
+}
