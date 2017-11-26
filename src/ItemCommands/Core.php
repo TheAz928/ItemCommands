653b2a51d0cd081e3f414d8a897ba754cffffafe
Tim (robske_110) <robske110@users.noreply.github.com>
2017-11-26 13:44:26 +0100

    Fix phpdocs and indenting
    That’s not how you do phpdocs!

--- src/ItemCommands/Core.php
+++ src/ItemCommands/Core.php
@@ -7,51 +7,47 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 
+use pocketmine\utils\TextFormat as TF;
+
 class Core extends PluginBase implements Listener{
 	
-	/* @var items (array)
-	 * contains Item::class (Not pocketmine Item::class)
+	/**
+	 * @var Item[] Not pocketmine\item\Item!
 	 */
 	public $items = [];
 	
 	public function onLoad(){
-		 $this->saveDefaultConfig();
-	    $this->getLogger()->info("§7Loading items...");
-	    foreach($this->getConfig()->getAll() as $stringKey => $data){
-	       $this->items[$stringKey] = new Item($stringKey, isset($data["lore"]) ? $data["lore"] : [], $data["name"], $data["commands"], $data["runAs"]);
-	       $this->getLogger()->info("§2Loaded ".$this->items[$stringKey]->getName());
-	    }
-	    $this->getLogger()->info("§aInit process done!");
+		$this->saveDefaultConfig();
+		$this->getLogger()->info(TF::GRAY."Loading items...");
+		foreach($this->getConfig()->getAll() as $stringKey => $data){
+			$this->items[$stringKey] = new Item($stringKey, isset($data["lore"]) ? $data["lore"] : [], $data["name"], $data["commands"], $data["runAs"]);
+			$this->getLogger()->info(TF::DARK_GREEN."Loaded ".$this->items[$stringKey]->getName());
+		}
 	}
 	
 	public function onEnable(){
-	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-	    $this->getLogger()->info("§6ItemCommands has been enabled successfully!");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	
-	/*
-	 * @void onHeld
-	 * PlayerItemHeldEvent $event
-	 * Priority: HIGHEST
+	/**
+	 * @param PlayerItemHeldEvent $event
+	 * @priority HIGHEST
 	 */
-	
-	public function onHeld(PlayerItemHeldEvent $event){
-	    $player = $event->getPlayer();
-	    foreach($this->items as $key => $class){
-	       $class->keepChecking($player);
-	    }
+	public function onHeld(PlayerItemHeldEvent $event): void{
+		$player = $event->getPlayer();
+		foreach($this->items as $key => $class){
+			$class->keepChecking($player);
+		}
 	}
 	
-	/*
-	 * @void onTap
-	 * PlayerInteractEvent $event
-	 * Priority: HIGHEST
+	/**
+	 * @param PlayerInteractEvent $event
+	 * @priority HIGHEST
 	 */
-	
-	public function onTap(PlayerInteractEvent $event){
-	    $player = $event->getPlayer();
-	    foreach($this->items as $key => $class){
-	       $class->checkExecution($player);
-	    }
+	public function onTap(PlayerInteractEvent $event): void{
+		$player = $event->getPlayer();
+		foreach($this->items as $key => $class){
+			$class->checkExecution($player);
+		}
 	}
 }
--- src/ItemCommands/Item.php
+++ src/ItemCommands/Item.php
@@ -7,168 +7,149 @@ use pocketmine\Server;
 use pocketmine\Player;
 
 /* 
- * Developed by TheAz928
- * ItemCommands handler class
- * This software is under GNU General Public License v3.0.0 and later
- */
+* Developed by TheAz928
+* ItemCommands handler class
+* This software is under GNU General Public License v3.0.0 or later
+*/
 
 use pocketmine\command\ConsoleCommandSender;
 
 class Item{
 
-   CONST AS_PLAYER = 0;
+	CONST AS_PLAYER = 0;
 
-   CONST AS_OP = 1;
+	CONST AS_OP = 1;
 
-   CONST AS_CONSOLE = 2;
+	CONST AS_CONSOLE = 2;
 	
-	/* @var stringKey (string) null|id:damage */
+	/** @var string|null (id:damage) */
 	private $stringKey = "";
 	
-	/* @var lore (array) */
+	/** @var array */
 	private $lore = [];
 	
-	/* @var name (string) */
+	/** @var string */
 	private $name = "";
 	
-	/* @var commands (array) */
+	/** @var array */
 	private $commands = [];
 	
-	/* @var runType (Int) */
+	/** @var int */
 	private $runType = 0;
 	
-	/*
+	/**
 	 * Item(CommandItem) constructor
 	 *
-	 * String $stringKey
-	 * Array $lore
-	 * String $name
-	 * Array $commands
-	 * String $runType
+	 * @param String $stringKey
+	 * @param array $lore
+	 * @param string $name
+	 * @param array $commands
+	 * @param int $runType
 	 */
-	
 	public function __construct(string $stringKey, array $lore, string $name, array $commands, Int $runType){
-	    $this->stringKey = $stringKey;
-       $this->lore = $lore;
-       $this->name = $name;
-       $this->commands = $commands;
-       $this->runType = $runType > 2 or $runType < 0 ? 0 : $runType;
+		$this->stringKey = $stringKey;
+		$this->lore = $lore;
+		$this->name = $name;
+		$this->commands = $commands;
+		$this->runType = $runType > 2 or $runType < 0 ? 0 : $runType;
 	}
 	
-	/*
-	 * @void getItem
-	 * return Item
+	/**
+	 * @return IT
 	 */
-	
 	public function  getItem(): IT{
-	    $data = explode(":", $this->stringKey);
-	    $meta = isset($data[1]) ? $data[1] : 0;
-	    $item = IT::get($data[0], $meta, 1);
-	return $item;
+		$data = explode(":", $this->stringKey);
+		$meta = isset($data[1]) ? $data[1] : 0;
+		$item = IT::get($data[0], $meta, 1);
+		return $item;
 	}
 	
-	/*
-	 * @void getLore
-	 * return array
+	/**
+	 * @return array
 	 */
-	
 	public function getLore(): array{
-	    $lore = [];
-	    foreach($this->lore as $lor){
-	       $lore[] = "§7".$lor."§r";
-	    }
-	return $lore;
+		$lore = [];
+		foreach($this->lore as $lor){
+			$lore[] = "§7".$lor."§r";
+		}
+		return $lore;
 	}
 	
-	/*
-	 * @void getName
-	 * return string
+	/**
+	 * @return string
 	 */
-	
 	public function getName(): string{
-	    $name = "§r".$this->name."§r";
-	return $name;
+		$name = "§r".$this->name."§r";
+		return $name;
 	}
 	
-	/*
-	 * @void getCommands
-	 * Player $player
-	 * return array
+	/**
+	 * @param Player $player
+	 * @return array
 	 */
-	
 	public function getCommands(Player $player): array{
-	    $cmds = [];
-	    foreach($this->commands as $key => $cmd){
-	      $cmds[] = str_replace(["{name}", "{tag}", "{level}", "{x}", "{y}", "{z}"], [$player->getName(), $player->getNameTag(), $player->getLevel()->getName(), $player->x, $player->y, $player->z], $cmd);
-	    }
-	return $cmds;
+		$cmds = [];
+		foreach($this->commands as $key => $cmd){
+			$cmds[] = str_replace(["{name}", "{tag}", "{level}", "{x}", "{y}", "{z}"], [$player->getName(), $player->getNameTag(), $player->getLevel()->getName(), $player->x, $player->y, $player->z], $cmd);
+		}
+		return $cmds;
 	}
 	
-	/*
-	 * @void getRunType
-	 * return string
+	/**
+	 * @return int
 	 */
-	
-	public function getRunType(): Int{
-	    return $this->runType;
+	public function getRunType(): int{
+		return $this->runType;
 	}
 	
-	/*
-	 * @void keepChecking
-	 * Player $player
+	/**
+	 * @param Player $player
 	 *
-	 * Checks player's inventory
-	 * if there is command item
-	 * but it doesn't have proper 
-	 * name or lore
-	 *
+	 * Checks player's inventory if there is this CommandItem
+	 * but it doesn't have proper name or lore.
 	 */
-	
 	public function keepChecking(Player $player): void{
-		 $check = $this->getItem();
-	    foreach($player->getInventory()->getContents() as $slot => $item){
-	       if($item->getId() == $check->getId() and $item->getDamage() == $check->getDamage()){
-		      if($item->getName() !== $this->getName() or $item->getLore() !== $this->getLore()){
-			     $item->setCustomName($this->getName());
-			     $item->setLore($this->getLore());
-			     $player->getInventory()->setItem($slot, $item);
-			    }
-		    }
-	    }
+		$check = $this->getItem();
+		foreach($player->getInventory()->getContents() as $slot => $item){
+			if($item->getId() == $check->getId() and $item->getDamage() == $check->getDamage()){
+				if($item->getName() !== $this->getName() or $item->getLore() !== $this->getLore()){
+					$item->setCustomName($this->getName());
+					$item->setLore($this->getLore());
+					$player->getInventory()->setItem($slot, $item);
+				}
+			}
+		}
 	}
 	
-	/*
-	 * @void checkExecution
-	 * Player $player
-	 *
+	/**
+	 * @param Player $player
 	 */
-	
 	public function checkExecution(Player $player): void{
-	    $inv = $player->getInventory();
-	    $item = $inv->getItemInHand();
-	    $check = $this->getItem();
-	    $hadOp = true;
-	    if($item->getId() == $check->getId() and $item->getDamage() == $check->getDamage()){
-		   if($item->getName() == $this->getName()){ # No need to check lore
-			  $commands = $this->getCommands($player);
-			  if($player->isOp() == false and $this->getRunType() == self::AS_OP){
-				 $hadOp = false;
-				 $player->setOp(true);
-			  }
-			  foreach($commands as $cmd){
-			    if($this->getRunType() == self::AS_PLAYER or $this->getRunType() == self::AS_OP){
-				   Server::getInstance()->dispatchCommand($player, $cmd);
-				 }
-				 if($this->getRunType() == self::AS_CONSOLE){
-				   Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), $cmd);
-				 }
-			  }
-			  if($hadOp == false){
-			   $player->setOp(false);
-			  }
-			  $item->setCount(1);
-			  $inv->removeItem($item);
-			  $player->sendTip("§7You've used ".$this->getName());
+		$inv = $player->getInventory();
+		$item = $inv->getItemInHand();
+		$check = $this->getItem();
+		$hadOp = true;
+		if($item->getId() == $check->getId() and $item->getDamage() == $check->getDamage()){
+			if($item->getName() == $this->getName()){ # No need to check lore
+				$commands = $this->getCommands($player);
+				if($player->isOp() == false and $this->getRunType() == self::AS_OP){
+					$hadOp = false;
+					$player->setOp(true);
+				}
+				foreach($commands as $cmd){
+					if($this->getRunType() == self::AS_PLAYER or $this->getRunType() == self::AS_OP){
+						Server::getInstance()->dispatchCommand($player, $cmd);
+					}
+					if($this->getRunType() == self::AS_CONSOLE){
+						Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), $cmd);
+					}
+				}
+				if($hadOp == false){
+					$player->setOp(false);
+				}
+				$item->setCount(1);
+				$inv->removeItem($item);
+				$player->sendTip("§7You've used ".$this->getName());
 			}
 		}
 	}
